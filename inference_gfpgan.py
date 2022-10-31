f421a77cd4d50a8cf1c19ffcbc45e14c0a2de221
hareemfatima10 <41821583+hareemfatima10@users.noreply.github.com>
2022-10-31 12:43:33 +0400

    function added

--- inference_gfpgan.py
+++ inference_gfpgan.py
@@ -9,6 +9,134 @@ from basicsr.utils import imwrite
 from gfpgan import GFPGANer
 
 
+def gfp_inference(input = 'inputs/whole_imgs', output='results', version='1.3', upscale=2,
+                 bg_upsampler='realesrgan', bg_tile=400, suffix=None, only_center_face=True, aligned=True,
+                 ext='auto', w=0.5):
+
+    # ------------------------ input & output ------------------------
+    if input.endswith('/'):
+        input = input[:-1]
+    if os.path.isfile(input):
+        img_list = [input]
+    else:
+        img_list = sorted(glob.glob(os.path.join(input, '*')))
+
+    os.makedirs(output, exist_ok=True)
+
+    # ------------------------ set up background upsampler ------------------------
+    if bg_upsampler == 'realesrgan':
+        if not torch.cuda.is_available():  # CPU
+            import warnings
+            warnings.warn('The unoptimized RealESRGAN is slow on CPU. We do not use it. '
+                          'If you really want to use it, please modify the corresponding codes.')
+            bg_upsampler = None
+        else:
+            from basicsr.archs.rrdbnet_arch import RRDBNet
+            from realesrgan import RealESRGANer
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
+            bg_upsampler = RealESRGANer(
+                scale=2,
+                model_path='https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth',
+                model=model,
+                tile=bg_tile,
+                tile_pad=10,
+                pre_pad=0,
+                half=True)  # need to set False in CPU mode
+    else:
+        bg_upsampler = None
+
+    # ------------------------ set up GFPGAN restorer ------------------------
+    if version == '1':
+        arch = 'original'
+        channel_multiplier = 1
+        model_name = 'GFPGANv1'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth'
+    elif version == '1.2':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANCleanv1-NoCE-C2'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.2.0/GFPGANCleanv1-NoCE-C2.pth'
+    elif version == '1.3':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANv1.3'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
+    elif version == '1.4':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANv1.4'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth'
+    elif version == 'RestoreFormer':
+        arch = 'RestoreFormer'
+        channel_multiplier = 2
+        model_name = 'RestoreFormer'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth'
+    else:
+        raise ValueError(f'Wrong model version {version}.')
+
+    # determine model paths
+    model_path = os.path.join('experiments/pretrained_models', model_name + '.pth')
+    if not os.path.isfile(model_path):
+        model_path = os.path.join('gfpgan/weights', model_name + '.pth')
+    if not os.path.isfile(model_path):
+        # download pre-trained models from url
+        model_path = url
+
+    restorer = GFPGANer(
+        model_path=model_path,
+        upscale=upscale,
+        arch=arch,
+        channel_multiplier=channel_multiplier,
+        bg_upsampler=bg_upsampler)
+
+    # ------------------------ restore ------------------------
+    for img_path in img_list:
+        # read image
+        img_name = os.path.basename(img_path)
+        print(f'Processing {img_name} ...')
+        basename, ext = os.path.splitext(img_name)
+        input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+        # restore faces and background if necessary
+        cropped_faces, restored_faces, restored_img = restorer.enhance(
+            input_img,
+            has_aligned=aligned,
+            only_center_face=only_center_face,
+            paste_back=True,
+            weight=weight)
+
+        # save faces
+        for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
+            # save cropped face
+            save_crop_path = os.path.join(output, 'cropped_faces', f'{basename}_{idx:02d}.png')
+            imwrite(cropped_face, save_crop_path)
+            # save restored face
+            if suffix is not None:
+                save_face_name = f'{basename}_{idx:02d}_{suffix}.png'
+            else:
+                save_face_name = f'{basename}_{idx:02d}.png'
+            save_restore_path = os.path.join(output, 'restored_faces', save_face_name)
+            imwrite(restored_face, save_restore_path)
+            # save comparison image
+            cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
+            imwrite(cmp_img, os.path.join(output, 'cmp', f'{basename}_{idx:02d}.png'))
+
+        # save restored img
+        if restored_img is not None:
+            if ext == 'auto':
+                extension = ext[1:]
+            else:
+                extension = ext
+
+            if suffix is not None:
+                save_restore_path = os.path.join(output, 'restored_imgs', f'{basename}_{suffix}.{extension}')
+            else:
+                save_restore_path = os.path.join(output, 'restored_imgs', f'{basename}.{extension}')
+            imwrite(restored_img, save_restore_path)
+
+    print(f'Results are in the [{output}] folder.')
+
+
 def main():
     """Inference demo for GFPGAN (for users).
     """
@@ -170,5 +298,5 @@ def main():
     print(f'Results are in the [{args.output}] folder.')
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
